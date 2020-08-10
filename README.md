91352f0766b8d6709d151482a564ef978fae5045
liukunhui <liukunhui@youcheyihou.com>
2020-08-10 23:15:09 +0800

    fix+docs: 完善细节+增加readme

---
+++ README.md
@@ -0,0 +1,9 @@
+### 用法
+```
+  npm i -g kbone-react-cli
+  kreact init
+```
+
+### 模版
+- kbone+typescript+redux+router
+- react+redux+router
--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const minimis = require('minimist');
 const yoemanEnv = require('yeoman-environment').createEnv();
 const resolveFrom = require('resolve-from').silent;
-const inquirer = require('inquirer');
 const install = require('./utils/install');
 const { getGenPath, sendConsle } = require('./utils/base');
 
 const argv = minimis(process.argv.slice(2));
 
 async function main() {
-  const { generator } = await inquirer.prompt({
-    message: '请选择一个模板：', type: 'list', name: 'generator', choices: ['generator-kreact'],
-  });
-
   sendConsle('下载模版中...');
-  await install(generator);
+  await install('generator-kreact');
   sendConsle('下载完成，开始安装', 'green');
 
-  yoemanEnv.register(resolveFrom(getGenPath(), generator), 'kreact');
+  yoemanEnv.register(resolveFrom(getGenPath(), 'generator-kreact'), 'kreact');
   yoemanEnv.run(['kreact'], (e, d) => {
     if (d) console.log('happy coding', 'green');
   });
